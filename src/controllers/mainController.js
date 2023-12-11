8df77814d59c889339077236179a994f1ed02c6a
Lucas Espantoso <espantoso.lucas@hotmail.com>
2023-12-10 22:51:05 -0300

    muestro producto en index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,11 +1,19 @@
-const index = (req, res) => {
-  const productos = [
+
+const model = require("../models/Producto");
+
+const index = async (req, res) => {
+ /*const productos = [
     // Se recuperan los datos de los productos en la base y se genera el json que se va a mostrar dinamicamente en el index o portada.
       { id: 1, nombre: "Producto 1" },
       { id: 2, nombre: "Producto 2" },
       { id: 3, nombre: "Producto 3" },
     ];
-    res.render("inicio", { mensaje: "Hola EJS", productos });
+*/
+
+const productos = await model.findAll();
+    res.render("inicio", { productos });
+
+
   };
   
   module.exports = {
